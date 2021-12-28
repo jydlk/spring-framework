e660f9c43dd6e8d6fba267d6fa5ac3522a8a7164
党良坤 <dangliangkun@jingyougroup.com>
2021-12-28 15:53:53 +0800

    '提交测试工程'

---
+++ spring-example/src/main/java/com/dlk/spring/demo/Car.java
@@ -0,0 +1,7 @@
+package com.dlk.spring.demo;
+
+public class Car {
+	public void run() {
+		System.out.println("running...");
+	}
+}
---
+++ spring-example/src/main/java/com/dlk/spring/demo/Test.java
@@ -0,0 +1,7 @@
+package com.dlk.spring.demo;
+
+public class Test {
+	public static void main(String[] args) {
+//		AnnotationApplicationContext context = new AnnotationApplicationContext();
+	}
+}
